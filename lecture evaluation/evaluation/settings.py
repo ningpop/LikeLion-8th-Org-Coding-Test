75210d1ca92b75a4d9b5715b6b2a6b4bc018059b
ningpop <craft1933@naver.com>
2020-02-24 04:45:09 +0900

    templates 상속 작업

--- lecture evaluation/evaluation/settings.py
+++ lecture evaluation/evaluation/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'evaluation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['evaluation/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
